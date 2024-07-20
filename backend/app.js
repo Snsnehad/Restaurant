22defcfefe060e22e7557da09d9135c952109098
Sneha <janhvi9415@gmail.com>
2024-07-20 22:29:45 +0530

    Cors

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {dbConnection} from './database/dbConnection.js'
+import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
-import reservtaionRouter from './routes/reservation.js'
+import reservtaionRouter from "./routes/reservation.js";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/reservation', reservtaionRouter);
+app.use("/reservation", reservtaionRouter);
 
 dbConnection();
 
